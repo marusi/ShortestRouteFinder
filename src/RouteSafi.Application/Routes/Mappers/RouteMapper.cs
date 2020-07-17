80e47eee2d4332eb90138a23ef823e7638a1ff60
Emanuel <emanuelfqueiroz@gmail.com>
2020-07-16 21:16:24 -0300

    changed to .NET 5

---
+++ src/RouteSafi.Application/Routes/Mappers/RouteMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RouteSafi.Application.Routes.Mappers
+{
+    public static class RouteMapper
+    {
+        public static IMapper mapper;
+        static RouteMapper()
+        {
+            mapper = new MapperConfiguration(cfg =>
+            {
+                
+            }).CreateMapper();
+        }
+
+        
+    }
+}
